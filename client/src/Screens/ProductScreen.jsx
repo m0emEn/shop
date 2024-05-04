b8cfc2ab392e4838d3e637b5f146e9f3a9b6cca2
m0emEn <moemensfaxi10@gmail.com>
2024-05-04 18:54:42 +0100

    redux setup

---
+++ client/src/Screens/ProductScreen.jsx
@@ -0,0 +1,41 @@
+import { Box, Center, Wrap, WrapItem } from "@chakra-ui/react";
+import ProductCard from "../components/ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts } from "../redux/actions/productsActions";
+const ProductScreen = () => {
+  const dispatch = useDispatch();
+  const { loading, error, products, pagination } = useSelector(
+    (state) => state.product
+  );
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+  return (
+    <>
+      {products.length > 1 && (
+        <Box>
+          <Wrap
+            spacing="30px"
+            justify={"center"}
+            minHeight={"80vh"}
+            mx={{ base: "12", md: "20", lg: "32" }}
+          >
+            {products.map((product) => {
+              return (
+                <WrapItem key={product._id}>
+                  <Center w="250px" height={"450px"}>
+                    <ProductCard product={product} loading={loading} />
+                  </Center>
+                </WrapItem>
+              );
+            })}
+          </Wrap>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;
